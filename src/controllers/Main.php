9160c74b9b818d83d9b3fea0c9a5489b9dbdf59d
jeditrick <kolya.tancor@gmail.com>
2015-09-12 23:25:19 +0300

    wrong variant but optimized

--- src/controllers/Main.php
+++ src/controllers/Main.php
@@ -1,7 +1,6 @@
 <?php
 namespace Home;
 
-use Abraham\TwitterOAuth\TwitterOAuth;
 use SSX\EpiTwitter;
 
 class Main
@@ -36,22 +35,34 @@ class Main
 
     public function getFriendsIds()
     {
-        return $this->connection->get_friendsIds(['screen_name' => $this->screenName])->ids;
+        //$this->connection->useAsynchronous(true);
+        $this->connection->setDebug(true);
+        $cursor = -1;
+        $ids = [];
+        do{
+            $content = $this->connection->get_friendsIds(['screen_name' => $this->screenName, 'cursor' => $cursor]);
+            $ids[] = $content->ids;
+            $cursor = $content->response['next_cursor'];
+        }while($cursor != 0);
+        return $ids;
     }
 
     public function getResultFeed()
     {
-
         $feed = [];
-        $users = $this->getFriendsIds();
-        $this->connection->useAsynchronous(true);
-        foreach ($users as $id) {
-            $info = $this->connection->get_usersShow(['user_id' => $id, 'include_entities' => false]);
-            $feed[$id]['info'] = [
-                'statuses_count' => $info->statuses_count,
-                'name' => $info->name,
-                'profile_image_url' => str_replace('_normal', '', $info->profile_image_url)
-            ];
+        $ids = $this->getFriendsIds();
+        //$this->connection->useAsynchronous(true);
+        foreach (array_chunk($ids,100) as $k=>$id_arr) {
+            $content = $this->connection->get_usersLookup(['user_id' => implode(',',$id_arr[$k]), 'include_entities' => false]);
+            foreach($content->response as $info){
+                $feed[$info['id']]['info'] = [
+                    'statuses_count' => $info['statuses_count'],
+                    'name' => $info['name'],
+                    'profile_image_url' => str_replace('_normal', '', $info['profile_image_url'])
+                ];
+            }
+
+
         }
         return $feed;
     }
