6eed596bea6b819b5f6d56636c9d716b329faecb
jeditrick <kolya.tancor@gmail.com>
2015-09-15 14:04:32 +0300

    fix error link

--- src/views/error.php
+++ src/views/error.php
@@ -2,10 +2,10 @@
 {% block content %}
 
 {% if error_code == 404%}
-    <h1>There is no info about this user ! Does he exist ????</h1><br/><a href="/uwc8">Go back</a>
+    <h1>There is no info about this user ! Does he exist ????</h1><br/><a href="/">Go back</a>
 {% elseif error_code == 429 %}
-    <h1>Rate limit exceeded!!! Please try again later</h1><br/><a href="/uwc8">Go back</a>
+    <h1>Rate limit exceeded!!! Please try again later</h1><br/><a href="/">Go back</a>
 {% else %}
-    <h1>Something goes wrong ! :( Try again</h1><br/><a href="/uwc8">Go back</a>
+    <h1>Something goes wrong ! :( Try again</h1><br/><a href="/">Go back</a>
 {% endif %}
 {% endblock %}
